1e283c0a222fa37f0da808a04653e98de67aaf6a
afternoon20 <shimadakyousuke@shimadakyousukenoMacBook-Air.local>
2026-06-12 23:15:17 +0900

    add a section

--- src/data/items.js
+++ src/data/items.js
@@ -44,6 +44,14 @@ export const morningItems = [
 export const nightItems = [
   {
     id: 1,
+    category: "クレンジングオイル(入浴前)",
+    name: "ピュア クレンジング オイル",
+    maker: "魔女工場",
+    img: "assets/images/night_001.jpg",
+    url: "https://manyo-japanese.com/Product/Detail/view/pid/302/cid/117",
+  },
+  {
+    id: 2,
     category: "化粧水",
     name: "ヘパトリート 薬用保湿化粧水",
     maker: "日本ゼトック",
@@ -51,7 +59,7 @@ export const nightItems = [
     url: "https://www.amazon.co.jp/dp/B0CNGR9D38",
   },
   {
-    id: 2,
+    id: 3,
     category: "美容液",
     name: "メラノCC プレミアム美容液",
     maker: "ロート製薬",
@@ -59,7 +67,7 @@ export const nightItems = [
     url: "https://www.shop.rohto.co.jp/category/skincare/melanocc/168583.html",
   },
   {
-    id: 3,
+    id: 4,
     category: "フェイスクリーム",
     name: "キュレル 潤浸保湿フェイスクリーム",
     maker: "花王",
@@ -67,11 +75,11 @@ export const nightItems = [
     url: "https://www.kao-kirei.com/ja/item/kbb/curel/4901301236210/?tw=kbb",
   },
   {
-    id: 4,
+    id: 5,
     category: "ニキビ治療薬",
     name: "エピデュオゲル",
     maker: "マルホ株式会社",
-    img: "assets/images/night_004.jpg",
+    img: "assets/images/night_005.jpg",
     url: "https://www.rohto.co.jp/",
   },
 ];
